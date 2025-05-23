7b8f9df57fd0d9529c41db4e75720527df13d860
Ryuu <144598428+ryuu-osei@users.noreply.github.com>
2025-05-23 12:12:45 -0500

    Add files via upload

---
+++ sem7/Views/formulario.php
@@ -0,0 +1,12 @@
+<form action="#" method="post">
+  <label for="nombre">Nombre:</label><br>
+  <input type="text" id="nombre" name="nombre" required><br><br>
+
+  <label for="correo">Correo:</label><br>
+  <input type="email" id="correo" name="correo" required><br><br>
+
+  <label for="comentarios">Comentarios:</label><br>
+  <textarea id="comentarios" name="comentarios" rows="4" cols="30" required></textarea><br><br>
+
+  <input type="submit" value="Enviar">
+</form>
---
+++ sem7/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Formulario de Contacto</title>
+</head>
+<body>
+  <h1>Bienvenido al Formulario</h1>
+  <?php require 'views/formulario.php'; ?>
+</body>
+</html>
